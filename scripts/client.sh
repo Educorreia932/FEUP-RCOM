2542790bbbd97048976ffc5636724f69bcaa1684
Ana Inês <up201806953@fe.up.pt>
2020-11-06 14:33:44 +0000

    Removed scripts & changed makefile

--- scripts/client.sh
+++
@@ -1,5 +0,0 @@
-cd ../src
-
-make clean & make
-
-./main -c "/dev/ttyS0" --baudrate B38400 --timeout 5 --num_transmissions 3 --alarm 3 --chunk_size 256 --file "../files/pinguim.gif"
--- scripts/initialize.sh
+++
@@ -1,1 +0,0 @@
-sudo socat -d -d PTY,link=/dev/ttyS10,mode=777 PTY,link=/dev/ttyS11,mode=777
